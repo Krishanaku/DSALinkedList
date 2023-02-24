0842da8eefeca332d231d94a83c21661e94313cc
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-24 15:52:41 +0530

    Refector : Ability to create Linked List by appending 30 and 70 to 56

--- Program.cs
+++ Program.cs
@@ -9,9 +9,9 @@ namespace DataStructure
             Console.WriteLine("Welcome to DSA LinkedList Problem Problem...");
 
             LinkedList linked = new LinkedList();
-            linked.add(56);
-            linked.add(30);
-            linked.add(70);
+            linked.Append(56); // add a elemt at last postion Append is nothinf act as Add () method
+            linked.Append(30);
+            linked.Append(70);
             linked.Display();
         }
     }
