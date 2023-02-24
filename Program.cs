2a03f4fbd63a8735a71a7189fcbd5634e6edacd1
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-24 14:41:05 +0530

    Add : Lets create a simple Linked List of 56, 30 and 70

--- Program.cs
+++ Program.cs
@@ -8,12 +8,11 @@ namespace DataStructure
         {
             Console.WriteLine("Welcome to DSA LinkedList Problem Problem...");
 
-
-
-
-
-
-
+            LinkedListDemo linked = new LinkedListDemo();
+            linked.add(50);
+            linked.add(30);
+            linked.add(70);
+            linked.Display();
         }
     }
 }
